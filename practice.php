2cc487e31bee0833420b87ddaec685ce9b138a98
osato <zazizuzezono@gmail.com>
2016-04-13 21:16:24 +0900

    式と答え

--- practice.php
+++ practice.php
@@ -1,25 +1,8 @@
 <?php
-echo "test1";
-echo "<br>";
-echo "<br>";
-for ($i = 1; $i <= 9; $i++){
-	echo $i;
-}
-
-
-echo "<br>";
-echo "<br>";
-for ($i = 1; $i <= 9; $i++){
-	$ans = 1 * $i;
- 	echo '1×'. $i.'='.$ans.'/';
-}
-
-
-echo "<br>";
-echo "<br>";
 for($a = 1; $a <=9; $a++){
 	for ($i = 1; $i <= 9; $i++){
 		$ans = $a * $i;
 		echo $a.'×'. $i.'='.$ans.'/';
 	}
+	echo "<br>";
 }
