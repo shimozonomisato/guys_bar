b9be5b863bc669eeea89810dfb18dffb7db4db28
osato <zazizuzezono@gmail.com>
2016-04-13 21:22:33 +0900

    HTMLを使って表示

--- practice.php
+++ practice.php
@@ -1,8 +1,12 @@
+<table border="1">
 <?php
 for($a = 1; $a <=9; $a++){
+	echo "<tr>";
 	for ($i = 1; $i <= 9; $i++){
 		$ans = $a * $i;
-		echo $a.'×'. $i.'='.$ans.'/';
+		echo "<td>".$a.'×'. $i.'='.$ans."</td>";
 	}
-	echo "<br>";
+	echo "</tr>";
 }
+?>
+</table>
