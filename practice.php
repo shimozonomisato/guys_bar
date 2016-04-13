692a8320d7a70a98a74d80000d3c3eae679e8069
osato <zazizuzezono@gmail.com>
2016-04-13 21:41:50 +0900

    偶数の段のみ表示

--- practice.php
+++ practice.php
@@ -1,10 +1,14 @@
 <table border="1">
 <?php
 for($a = 1; $a <=9; $a++){
+	if($a % 2 == 0){
 	echo "<tr>";
 	for ($i = 1; $i <= 9; $i++){
 		$ans = $a * $i;
 		echo "<td>".$a.'×'. $i.'='.$ans."</td>";
+	}
+	}else{
+		echo "";
 	}
 	echo "</tr>";
 }
