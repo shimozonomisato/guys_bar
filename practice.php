f80c2575f368a3b10f6186f4e58cfd600252f825
osato <zazizuzezono@gmail.com>
2016-04-13 21:45:31 +0900

    偶数✖️偶数の時だけ表示

--- practice.php
+++ practice.php
@@ -4,8 +4,12 @@ for($a = 1; $a <=9; $a++){
 	if($a % 2 == 0){
 	echo "<tr>";
 	for ($i = 1; $i <= 9; $i++){
+		if($i % 2 == 0){
 		$ans = $a * $i;
 		echo "<td>".$a.'×'. $i.'='.$ans."</td>";
+		}else{
+		echo "";
+	}
 	}
 	}else{
 		echo "";
