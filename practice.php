32484786ab524bbb801072572a2032a57bb634ee
osato <zazizuzezono@gmail.com>
2016-04-14 23:09:07 +0900

    答えが奇数の時だけ表示

--- practice.php
+++ practice.php
@@ -1,12 +1,13 @@
-<table border="1">
+<table border="1" >
 <?php
 for($a = 1; $a <=9; $a++){
 	echo "<tr>";
 	for ($i = 1; $i <= 9; $i++){
 		$ans = $a * $i;
-		if($ans % 2 == 0){
-			echo "<td>".$a.'×'. $i.'='.$ans."</td>";
-		}else{echo "<td>".$nbsp."</td>";
+		if($ans % 2 == 1){
+			echo "<td width=150 height=30 align=center>".$a.'×'. $i.'='.$ans."</td>";
+		}else{
+			echo "<td width=150 height=30>".$nbsp."</td>";
 		}
 	}
 	echo "</tr>";	
