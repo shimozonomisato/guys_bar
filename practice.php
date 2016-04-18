75fd864288c4b0048a89a5ea6f64a6729afb8d14
osato <zazizuzezono@gmail.com>
2016-04-18 23:13:30 +0900

    配列で九九表＿北村さん

--- practice.php
+++ practice.php
@@ -1,6 +1,9 @@
 <table border="1">
 <?php
-$data = array(1,2,3,4,5,6,7,8,9);
+//$data = array(1,2,3,4,5,6,7,8,9);
+for($a=1; $a <= 100; $a++){
+	$data[$a-1]=$a;
+}
 for($a=1; $a <= count($data); $a++){
 	echo"<tr><td>".$a."</td></tr><tr>";
 	for($i = 1; $i <= count($data); $i++){
